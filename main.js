83cecec9510ebed3bc3d9427e0cd4d2847aa4ee7
Raul Jimenez <rjbarco@gmail.com>
2023-01-12 07:23:44 +0100

    'units' global variable added. Call to the API includes it now

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+let units = "metric"; // Change it to "imperial" for Fahrenheit grades
+
 async function getWeatherData(location) {
     try {
         // Com with the server
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=a9d46371cffec0745195f9b5d7ae205b`, {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=a9d46371cffec0745195f9b5d7ae205b&units=${units}`, {
             mode: 'cors'
         });
         // Passing from object to json format
