087794b5902ba3833311b186a6837489e65bcbfb
Raul Jimenez <rjbarco@gmail.com>
2023-01-16 09:56:04 +0100

    Included functions to convert units. Class Weather constructor created.

--- main.js
+++ main.js
@@ -1,4 +1,86 @@
+// Global variables and constants
 let units = "metric"; // Change it to "imperial" for Fahrenheit grades
+
+// This function transform wind direction in degrees unit to coordinates based in 32 points rose compass
+function degreesToCoordinates(degrees) {
+    if (degrees >= 348.75 || degrees < 11.25) {
+        return "N";
+    } else if (degrees >= 11.25 && degrees < 33.75) {
+        return "NNE";
+    } else if (degrees >= 33.75 && degrees < 56.25) {
+        return "NE";
+    } else if (degrees >= 56.25 && degrees < 78.75) {
+        return "ENE";
+    } else if (degrees >= 78.75 && degrees < 101.25) {
+        return "E";
+    } else if (degrees >= 101.25 && degrees < 123.75) {
+        return "ESE";
+    } else if (degrees >= 123.75 && degrees < 146.25) {
+        return "SE";
+    } else if (degrees >= 146.25 && degrees < 168.75) {
+        return "SSE";
+    } else if (degrees >= 168.75 && degrees < 191.25) {
+        return "S";
+    } else if (degrees >= 191.25 && degrees < 213.75) {
+        return "SSW";
+    } else if (degrees >= 213.75 && degrees < 236.25) {
+        return "SW";
+    } else if (degrees >= 236.25 && degrees < 258.75) {
+        return "WSW";
+    } else if (degrees >= 258.75 && degrees < 281.25) {
+        return "W";
+    } else if (degrees >= 281.25 && degrees < 303.75) {
+        return "WNW";
+    } else if (degrees >= 303.75 && degrees < 326.25) {
+        return "NW";
+    } else if (degrees >= 326.25 && degrees < 348.75) {
+        return "NNW";
+    } else {
+        return "Invalid Value";
+    }
+}
+
+// This function translate Unix time into HH:MM:SS format
+function unixTimeToHHMMSS(time) {
+    // input unit is mili seconds and we need seconds instead
+    var date = new Date(time * 1000);
+    
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes + ':' + seconds;
+
+    return formattedTime;
+}
+
+// Weather Class
+class Weather {
+    constructor(location,country,timezone,title,desc,icon,temp,feels,pressure,humidity,windSpeed,windDeg,rain1,snow1,clouds,utcTime,sunrise,sunset) {
+        this.location = location;
+        this.country = country;
+        this.timezone = timezone / 3600; // From seconds to hours
+        this.title = title;
+        this.desc = desc;
+        this.icon = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        this.temp = temp;
+        this.feels = feels;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+        this.windDeg = degreesToCoordinates(windDeg); // Call to custom function
+        this.rain1 = rain1;
+        this.snow1 = snow1;
+        this.clouds = clouds;
+        this.utcTime = unixTimeToHHMMSS(utcTime);
+        this.sunrise = unixTimeToHHMMSS(sunrise);
+        this.sunset = unixTimeToHHMMSS(sunset);
+    }
+}
 
 async function getWeatherData(location) {
     try {
